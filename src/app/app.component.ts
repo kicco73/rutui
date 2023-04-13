a72f02f91d2fffd59787f1fd824f83e358efecc9
Enrico Carniani <>
2023-04-13 19:10:27 +0200

    - first version of the tbx to lemon interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { FileUpload } from 'primeng/fileupload';
+
+type Sparql = {
+  fileSize: number;
+  tbxType: string;
+  numberOfConcepts: number;
+  numberOfTerms: number;
+  numberOfLanguages: number;
+  content: String;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rutui';
+  title : string = 'rutui';
+  tbx : string = '';
+  sparql : Sparql | null = null;
+
+  async onUpload(event: FileUpload) {
+    for(let file of event.files) {
+      this.tbx = await file.text();
+      this.onConvert();
+    }
+  }
+
+  async onConvert() {
+    console.log('ciao');
+    fetch("http://localhost:8080/convert", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "tbx": this.tbx })
+    })
+    .then(response => response.json())
+    .then(sparql => this.sparql = sparql)
+    .catch(fail => console.error(fail));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabViewModule } from 'primeng/tabview';
+import { PanelModule } from 'primeng/panel';
+
+//import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'hello', component: HelloworldComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, RouterModule.forRoot(routes),
+    ButtonModule, FileUploadModule, ScrollPanelModule, TabViewModule, PanelModule],
+  declarations: [AppComponent, HelloworldComponent],
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }

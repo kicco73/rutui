f479728c4ad33ada03a65ccde7697ff08347de5c
Enrico Carniani <>
2023-05-16 15:26:51 +0200

    using first label encountered

--- src/app/filter-view/filter-view.component.ts
+++ src/app/filter-view/filter-view.component.ts
@@ -14,7 +14,7 @@ export class FilterViewComponent implements OnChanges {
 
   getLabel(concept: Concept): string | undefined {
     for (let language of Object.keys(concept.languages)) {
-      return `${concept.languages[language].terms![0].t} (${concept.id})`;
+      return `${concept.languages[language].label} (${concept.id})`;
     }
     return undefined;
   }
